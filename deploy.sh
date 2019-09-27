a0ae8bc361a1e945c19ad539cb1445abcf1f7f22
Onur Aslan <aslanon.tr@gmail.com>
2019-09-27 12:42:13 +0300

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/aslanon/giphy-search.git master:gh-pages
+git push -f https://github.com/aslanon/vue-dropdown.git master:gh-pages
 
 cd -
